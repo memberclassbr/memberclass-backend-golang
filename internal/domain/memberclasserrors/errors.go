35fea1e163a02a9ef82c2a1b1e81c5fbbc4a27ff
Alef-Daniel <alefsa_daniel@hotmail.com>
2025-09-25 01:34:48 -0300

    feat: add tenant not found error

--- internal/domain/memberclasserrors/errors.go
+++ internal/domain/memberclasserrors/errors.go
@@ -10,9 +10,14 @@ func (e *MemberClassError) Error() string {
 	return e.Message
 }
 
-func NewMemberClassError(code int, message string, err error) error {
+func NewMemberClassError(code int, message string) error {
 	return &MemberClassError{
 		Code:    code,
 		Message: message,
 	}
 }
+
+var (
+	ErrTenantNotFound = NewMemberClassError(404, "tenant not found")
+	ErrTenantIDEmpty  = NewMemberClassError(400, "tenant ID is empty")
+)
