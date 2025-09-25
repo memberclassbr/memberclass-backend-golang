b0f9556cacde3231f4ff39bdd6eb447c62dd95df
Alef-Daniel <alefsa_daniel@hotmail.com>
2025-09-24 22:21:26 -0300

    feat: init infrastructure to database

---
+++ internal/infrastructure/adapters/database/factory.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"database/sql"
+	"os"
+)
+
+func NewDB() (*sql.DB, error) {
+	driver := os.Getenv("DB_DRIVER")
+	dsn := os.Getenv("DB_DSN")
+
+	db, err := sql.Open(driver, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
