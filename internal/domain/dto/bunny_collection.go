2414c74dd6e382c36db613584a2910ef985bc07b
Alef-Daniel <alefsa_daniel@hotmail.com>
2025-09-25 01:35:57 -0300

    refactor: remove duplicate bunny collection DTO

--- internal/domain/dto/bunny_collection.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-type BunnyCollection struct {
-	GUID       string `json:"guid"`
-	Name       string `json:"name"`
-	VideoCount int    `json:"videoCount"`
-	CreatedAt  string `json:"createdAt"`
-	UpdatedAt  string `json:"updatedAt"`
-}
