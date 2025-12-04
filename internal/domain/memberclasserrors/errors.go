14d015b3d36c2662ffa502f45e3a4763b645bcb9
Alef-Daniel <alef.adaniel@outlook.com>
2025-12-03 22:27:57 -0300

    feat(errors): add ErrUserNotFound error

--- internal/domain/memberclasserrors/errors.go
+++ internal/domain/memberclasserrors/errors.go
@@ -20,10 +20,17 @@ func NewMemberClassError(code int, message string) error {
 }
 
 var (
-
 	//Errors Tenant
 	ErrTenantNotFound = NewMemberClassError(404, "tenant not found")
 	ErrTenantIDEmpty  = NewMemberClassError(400, "tenant ID is empty")
+
+	//Errors User
+	ErrUserNotFound        = NewMemberClassError(404, "user not found")
+	ErrUserNotInTenant     = NewMemberClassError(403, "user does not belong to this tenant")
+	ErrUnauthorized        = NewMemberClassError(401, "unauthorized")
+	ErrSessionTokenMissing = NewMemberClassError(401, "session token not found")
+	ErrSessionTokenInvalid = NewMemberClassError(401, "invalid session token")
+	ErrSessionExpired      = NewMemberClassError(401, "session expired")
 
 	//Errors Lessons
 	ErrLessonNotFound   = errors.New("lesson not found")
