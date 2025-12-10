8308da3dff76ef797b69a2d92dcdf5b228208771
Alef-Daniel <alef.adaniel@outlook.com>
2025-12-10 16:50:37 -0300

    feat: add FindByEmail method to UserRepository interface
    Add method to find user by email address for user activity validation

--- internal/domain/ports/user_repository.go
+++ internal/domain/ports/user_repository.go
@@ -4,6 +4,7 @@ import "github.com/memberclass-backend-golang/internal/domain/entities"
 
 type UserRepository interface {
 	FindByID(userID string) (*entities.User, error)
+	FindByEmail(email string) (*entities.User, error)
 	ExistsByID(userID string) (bool, error)
 	BelongsToTenant(userID string, tenantID string) (bool, error)
 }
