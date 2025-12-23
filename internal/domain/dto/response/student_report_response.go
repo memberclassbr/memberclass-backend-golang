f8e83086d4552a9a000e5e2fc53cef4a93568801
Alef-Daniel <alef.adaniel@outlook.com>
2025-12-23 19:40:41 -0300

    feat: add DTOs for student report endpoint

---
+++ internal/domain/dto/response/student_report_response.go
@@ -0,0 +1,33 @@
+package response
+
+type LessonWatched struct {
+	AulaID      string `json:"aula_id"`
+	Titulo      string `json:"titulo"`
+	DataAssistida string `json:"data_assistida"`
+}
+
+type StudentReport struct {
+	AlunoIDMemberClass        string         `json:"aluno_id_member_class"`
+	Email                     string         `json:"email"`
+	Cpf                       string         `json:"cpf"`
+	DataCadastro              string         `json:"data_cadastro"`
+	EntregasVinculadas        []string       `json:"entregas_vinculadas"`
+	UltimoAcesso              *string         `json:"ultimo_acesso"`
+	QuantidadeAulasAssistidas int            `json:"quantidade_aulas_assistidas"`
+	AulasAssistidas           []LessonWatched `json:"aulas_assistidas"`
+}
+
+type StudentReportPagination struct {
+	Page        int  `json:"page"`
+	Limit       int  `json:"limit"`
+	TotalCount  int  `json:"totalCount"`
+	TotalPages  int  `json:"totalPages"`
+	HasNextPage bool `json:"hasNextPage"`
+	HasPrevPage bool `json:"hasPrevPage"`
+}
+
+type StudentReportResponse struct {
+	Alunos     []StudentReport          `json:"alunos"`
+	Pagination StudentReportPagination  `json:"pagination"`
+}
+
